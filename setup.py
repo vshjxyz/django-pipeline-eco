08f8ae325f1fa9579ff59b35a33dbbe64ae69df5
Luca Del Bianco <vshjxyz@gmail.com>
2012-09-30 16:07:52 +0200

    Fixing the requirements, adding the correct README as the long_description

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-description = """
-django-pipeline compiler for eco templates
-"""
+with open('README.rst') as file:
+    description = file.read()
 
 setup(
     name='django-pipeline-eco',
-    version='0.1.0',
-    description=description,
+    version='0.1.2',
+    description='django-pipeline compiler for eco templates',
     long_description=description,
     author='Luca Del Bianco',
     author_email='vshjxyz@gmail.com',
     url='https://github.com/vshjxyz/django-pipeline-eco',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['django-pipeline'],
+    install_requires=[
+        'django-pipeline',
+        'eco',
+    ],
     include_package_data=True,
     classifiers=[
         'Environment :: Web Environment',
